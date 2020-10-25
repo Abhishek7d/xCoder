ad98803fa68d157c3adfbb8618c85e3ff5b2519f
Dibyendu Saha <dsaha1656@gmail.com>
2020-10-25 20:27:51 +0000

    change api end point

--- frontend/src/model/config.js
+++ frontend/src/model/config.js
@@ -1,2 +1,2 @@
-// export const apiUrl = "http://157.245.252.144/api" //for PRODUCTION
-export const apiUrl = "http://127.0.0.1:8000/api" //for DEVELOPMENT
+export const apiUrl = "http://parvaty.me/api" //for PRODUCTION
+// export const apiUrl = "http://127.0.0.1:8000/api" //for DEVELOPMENT
