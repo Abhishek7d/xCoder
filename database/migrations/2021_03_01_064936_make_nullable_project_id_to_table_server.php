1b22171722ff0da24f8ff90c4c7a97ea07770dce
ashok64554 <ashok@nrt.co.in>
2021-03-01 12:21:52 +0530

    migration

---
+++ database/migrations/2021_03_01_064936_make_nullable_project_id_to_table_server.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use phpDocumentor\Reflection\Types\Nullable;
+
+class MakeNullableProjectIdToTableServer extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('servers', function (Blueprint $table) {
+            $table->bigInteger('project_id')->nullable()->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('servers', function (Blueprint $table) {
+            //
+        });
+    }
+}
